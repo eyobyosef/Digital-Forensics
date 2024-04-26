7db658e28655941cbade6f331a5ed68c73ca9332
Eyob T. Yosef <147551619+eyobyosef@users.noreply.github.com>
2024-04-26 14:01:04 -0400

    Create exify.csv.py

---
+++ exify.csv.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import os
+import csv
+from PIL import Image
+from PIL.ExifTags import GPSTAGS, TAGS
+
+# Helper function
+def create_google_maps_url(gps_coords):
+    dec_deg_lat = convert_decimal_degrees(
+        float(gps_coords["lat"][0]),
+        float(gps_coords["lat"][1]),
+        float(gps_coords["lat"][2]),
+        gps_coords["lat_ref"]
+    )
+    dec_deg_lon = convert_decimal_degrees(
+        float(gps_coords["lon"][0]),
+        float(gps_coords["lon"][1]),
+        float(gps_coords["lon"][2]),
+        gps_coords["lon_ref"]
+    )
+    return f"https://maps.google.com/?q={dec_deg_lat},{dec_deg_lon}"
+
+def convert_decimal_degrees(degree, minutes, seconds, direction):
+    decimal_degrees = degree + minutes / 60 + seconds / 3600
+    if direction == "S" or direction == "W":
+        decimal_degrees *= -1
+    return decimal_degrees
+
+
+
+cwd = os.getcwd()
+os.chdir("/home/eyob/Desktop/DF")
+files = os.listdir()
+
+with open("../exif_data.csv", "a", newline="") as csv_file:
+    writer = csv.writer(csv_file)
+
+    for file in files:
+        try:
+            image = Image.open(file)
+            writer.writerow(("Filename", file))
+            gps_coords = {}
+            if image._getexif() == None:
+                writer.writerow((file, "Contains no EXIF data."))
+            else:
+                for tag, value in image._getexif().items():
+                    tag_name = TAGS.get(tag)
+                    if tag_name == "GPSInfo":
+                        for key, val in value.items():
+                            writer.writerow((GPSTAGS.get(key), val))
+                            if GPSTAGS.get(key) == "GPSLatitude":
+                                gps_coords["lat"] = val
+                            elif GPSTAGS.get(key) == "GPSLongitude":
+                                gps_coords["lon"] = val
+                            elif GPSTAGS.get(key) == "GPSLatitudeRef":
+                                gps_coords["lat_ref"] = val
+                            elif GPSTAGS.get(key) == "GPSLongitudeRef":
+                                gps_coords["lon_ref"] = val   
+                    else:
+                        writer.writerow((tag_name, value))
+                if gps_coords:
+                    writer.writerow(("Google Maps Link", create_google_maps_url(gps_coords)))
+        except IOError:
+            print("File format not supported!")
+
+os.chdir(cwd)
